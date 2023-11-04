9a226e74c19a97772f89c883ca1addf1ac0e2c28
GreenBeansAgainstVeganism <kradrling@gmail.com>
2023-11-04 11:48:37 -0700

    more structure & tidyness

--- scripts/game.js
+++ scripts/game.js
@@ -1,19 +1,53 @@
-let dom_main;
 import screens from './screens.js';
+let dom_main;
+let activeScreenId = screens.home.id;
 
 // This event fires once the page is fully loaded, any code which reads/modifies the page data must be called after the page is loaded.
 window.addEventListener('load', event => {
+    // Populate global DOM variables
     dom_main = document.querySelector('main');
-    goto_home();
 
-    document.querySelector('h1').addEventListener('click',() => {goto_home()});
+    // Set screen to home
+    render_home();
+
+    // Make the title text link to the home screen
+    document.querySelector('h1').addEventListener('click',() => {render_home()});
 });
 
-function goto_home() {
-    dom_main.innerHTML = screens.home;
-    document.getElementById('button-begin').addEventListener('click',() => {goto_characterSelect()});
+/* -------------------------------------------------------------------------- */
+/*                               Event handlers                               */
+/* -------------------------------------------------------------------------- */
+
+function handle_scroll_characters()
+{
+    if(activeScreenId != screens.home.id) throw new Error("Character screen not active");
+
+    // TODO: implement
 }
 
-function goto_characterSelect() {
-    dom_main.innerHTML = screens.characterSelect;
+/* -------------------------------------------------------------------------- */
+/*                Rendering functions (because who needs React)               */
+/* -------------------------------------------------------------------------- */
+
+/**
+ * Renders the home screen
+ */
+function render_home() {
+    // overwrite contents of main
+    dom_main.innerHTML = screens.home.htmlContent;
+    activeScreenId = screens.home.id;
+
+    // Must attach all event listeners here because js modules are not accessible from the html
+    dom_main.querySelector('#button-begin').addEventListener('click',() => {render_characterSelect()});
+}
+
+/**
+ * Renders the character select screen
+ */
+function render_characterSelect() {
+    // overwrite contents of main
+    dom_main.innerHTML = screens.characterSelect.htmlContent;
+    activeScreenId = screens.characterSelect.id;
+    
+    // TODO: add listeners for character list scrolling
 }
--- scripts/screens.js
+++ scripts/screens.js
@@ -1,5 +1,17 @@
+class Screen {
+    #id_counter = 0;
+    constructor(htmlContent)
+    {
+        this.htmlContent = htmlContent;
+        this.id = this.#id_counter++;
+    }
+}
+
+/**
+ * Stores all the skeleton html for the different game screens. Located in a seperate module to avoid cluttering up the code
+ */
 let screens = {
-    home: `<div id="exposition">
+    home: new Screen(`<div id="exposition">
     <p>
         <strong>1 in every 36 children</strong> is diagnosed with <strong>Autism Spectrum Disorder</strong> (ASD) as of 2022. They strain themselves every day just to fit in to our society, yet <strong>we understand very little</strong> about what life is like for them.
     </p>
@@ -11,33 +23,38 @@ let screens = {
     <button id="button-begin">
         Begin
     </button>
-    </div>`,
-    characterSelect: `
+    </div>`),
+    characterSelect: new Screen(`
     <div id="exposition">
-        <p>
-            Select your story:
-        </p>
+    <p>
+        Select your story:
+    </p>
+    <p>
+        (Click a card to see more info)
+    </p>
+</div>
+<div id="character-select">
+    <div id="character-list">
+        <div class="character-card">
+            <img src="./assets/blank_character_icon.png" alt="blank character icon">
+            <h2>
+                Kevin
+            </h2>
+        </div>
+        <div class="character-card">
+            <img src="./assets/blank_character_icon.png" alt="blank character icon">
+            <h2>
+                Kevin
+            </h2>
+        </div>
+        <div class="character-card">
+            <img src="./assets/blank_character_icon.png" alt="blank character icon">
+            <h2>
+                Kevin
+            </h2>
+        </div>
     </div>
-    <div id="options">
-        <div class="character-card">
-            <img src="./assets/blank_character_icon.png" alt="blank character icon">
-            <h2>
-                Kevin
-            </h2>
-        </div>
-        <div class="character-card">
-            <img src="./assets/blank_character_icon.png" alt="blank character icon">
-            <h2>
-                Kevin
-            </h2>
-        </div>
-        <div class="character-card">
-            <img src="./assets/blank_character_icon.png" alt="blank character icon">
-            <h2>
-                Kevin
-            </h2>
-        </div>
-    </div>`
+</div>`)
 };
 
 export default screens;
