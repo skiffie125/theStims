b818bd490a24576c51d803c208af7b51cb4205ae
GreenBeansAgainstVeganism <kradrling@gmail.com>
2023-11-03 23:05:07 -0700

    home and character screen skeleton

--- scripts/game.js
+++ scripts/game.js
@@ -1,1 +1,19 @@
-console.log("hello world!");
+let dom_main;
+import screens from './screens.js';
+
+// This event fires once the page is fully loaded, any code which reads/modifies the page data must be called after the page is loaded.
+window.addEventListener('load', event => {
+    dom_main = document.querySelector('main');
+    goto_home();
+
+    document.querySelector('h1').addEventListener('click',() => {goto_home()});
+});
+
+function goto_home() {
+    dom_main.innerHTML = screens.home;
+    document.getElementById('button-begin').addEventListener('click',() => {goto_characterSelect()});
+}
+
+function goto_characterSelect() {
+    dom_main.innerHTML = screens.characterSelect;
+}
---
+++ scripts/screens.js
@@ -0,0 +1,43 @@
+let screens = {
+    home: `<div id="exposition">
+    <p>
+        <strong>1 in every 36 children</strong> is diagnosed with <strong>Autism Spectrum Disorder</strong> (ASD) as of 2022. They strain themselves every day just to fit in to our society, yet <strong>we understand very little</strong> about what life is like for them.
+    </p>
+    <p>
+        Are <strong>you</strong> willing to take a walk in their shoes?
+    </p>
+    </div>
+    <div id="options">
+    <button id="button-begin">
+        Begin
+    </button>
+    </div>`,
+    characterSelect: `
+    <div id="exposition">
+        <p>
+            Select your story:
+        </p>
+    </div>
+    <div id="options">
+        <div class="character-card">
+            <img src="./assets/blank_character_icon.png" alt="blank character icon">
+            <h2>
+                Kevin
+            </h2>
+        </div>
+        <div class="character-card">
+            <img src="./assets/blank_character_icon.png" alt="blank character icon">
+            <h2>
+                Kevin
+            </h2>
+        </div>
+        <div class="character-card">
+            <img src="./assets/blank_character_icon.png" alt="blank character icon">
+            <h2>
+                Kevin
+            </h2>
+        </div>
+    </div>`
+};
+
+export default screens;
